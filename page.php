bc8526280c677fcef3b841bd0ca943d9f6183c96
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2021-12-20 16:45:34 -0300

    2021_12_20_16_45_33

---
+++ page.php
@@ -0,0 +1,55 @@
+﻿<?php get_header(); ?>
+                    <!--/.span3-->
+                    <div class="span9">
+                        <div class="content">
+                   
+                            <!--/#btn-controls-->
+  
+                            <!--/.module-->
+                            <div class="btn-controls">
+                                <div class="btn-box-row row-fluid">
+                                    
+                                </div>
+                                <div class="btn-box-row row-fluid">
+                   
+                          
+                                </div>
+                            </div>
+                            <!--/#btn-controls-->
+                            <div class="module">
+                     
+              
+                            </div>
+                            <!--/.module-->
+                            <div class="module hide">
+                
+                 
+                            </div>
+                            <div class="module">
+                                <div class="module-head">
+                                <h3>DataTables</h3>
+                                </div>
+                                <div class="module-body table">
+                      
+<?php the_content(); ?>
+
+
+
+                                </div>
+                            </div>
+                            <!--/.module-->
+
+
+
+
+
+
+
+
+
+                            <!--/.module-->
+                        </div>
+                        <!--/.content-->
+                    </div>
+                    <!--/.span9-->
+<?php get_footer(); ?>               
